235b17ccb5725c834275da7e0b33da260793b24b
Michael Opara <opara@college.harvard.edu>
2018-05-05 12:51:21 -0400

    commit at Sat May  5 12:51:21 EDT 2018

--- src/91r.py
+++ src/91r.py
@@ -9,18 +9,18 @@ def parse_args():
 
   parser.add_argument("--seed", default=1, type=int, metavar="S",
     help="random seed (default: 1)")
-  parser.add_argument("--shuffle", action="store_true", default=False,
+  parser.add_argument("-s", "--shuffle", action="store_true", default=False,
     help="shuffle training data (default: false)")
-  parser.add_argument("--batch-size", default=128, type=int,
+  parser.add_argument("-b", "--batch-size", default=128, type=int,
     help="input batch size for training (default: 128)", metavar="N")
-  parser.add_argument("--num-epochs", default=10, type=int,
+  parser.add_argument("-n", "--num-epochs", default=10, type=int,
     help="number of epochs to train (default: 10)", metavar="E")
   parser.add_argument("--l1", default=0, type=float,
     help="L1 regularization coefficient (default: 0)", metavar="L1")
-  parser.add_argument("--no-cuda", action="store_true", default=False,
-    help="disable CUDA training")
-  parser.add_argument("--test", type=str, help="testing dataset", metavar="X")
-  parser.add_argument("--train", type=str, required=True,
+  parser.add_argument("-c", "--cuda", action="store_true", default=False,
+    help="enable CUDA training")
+  parser.add_argument("-i", "--test", type=str, help="testing dataset", metavar="X")
+  parser.add_argument("-j", "--train", type=str, required=True,
     help="training dataset", metavar="X")
 
   return parser.parse_args()
@@ -72,7 +72,7 @@ if __name__ == "__main__":
 
   T.manual_seed(args.seed)
 
-  device = T.device("cpu" if args.no_cuda else "cuda")
+  device = T.device("cuda" if args.cuda else "cpu")
 
   X_trn, Xf_trn, height, width, D_in = data(args.train, device)
 
