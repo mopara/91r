b78c1a85f7dd2de09d99fb42e1037b6f4662563a
Michael Opara <opara@college.harvard.edu>
2018-05-05 16:01:15 -0400

    commit at Sat May  5 16:01:15 EDT 2018

---
+++ src/keras_ae.py
@@ -0,0 +1,30 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+# from keras.datasets import mnist
+import numpy as np
+
+x_train = np.load("~/91r/mnist/train-images-idx3-ubyte.T")
+x_test = np.load("~/91r/mnist/t10k-images-idx3-ubyte.T")
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+input_img = Input(shape=(784,))
+encoded = Dense(128, activation='relu')(input_img)
+encoded = Dense(64, activation='relu')(encoded)
+encoded = Dense(32, activation='relu')(encoded)
+
+decoded = Dense(64, activation='relu')(encoded)
+decoded = Dense(128, activation='relu')(decoded)
+decoded = Dense(784, activation='sigmoid')(decoded)
+
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+autoencoder.fit(x_train, x_train,
+                epochs=100,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
