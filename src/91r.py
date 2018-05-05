0199acbef348f4401410676a7fd0d1a423bb2b09
Michael Opara <opara@college.harvard.edu>
2018-05-05 13:28:22 -0400

    commit at Sat May  5 13:28:22 EDT 2018

--- src/91r.py
+++ src/91r.py
@@ -49,7 +49,8 @@ def train(model, batches, num_epochs):
       loss2 += batch_l2
 
       # loss += batch_loss * X.size(0)
-      loss += batch_l2
+      loss += batch_l1
+      # loss += batch_l2
 
       model.opt.zero_grad()
       batch_loss.backward()
