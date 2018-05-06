1521ba0da7970f088e5677b58498d10759c7250d
Michael Opara <opara@college.harvard.edu>
2018-05-05 22:27:56 -0400

    commit at Sat May  5 22:27:56 EDT 2018

---
+++ src/data.py
@@ -0,0 +1,73 @@
+import argparse
+import cv2
+import os.path as path
+import numpy as np
+import struct
+import torch as T
+
+def parse_args():
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument("action", type=str, help="either 'mnist' or 'vids'")
+  parser.add_argument("-i", nargs="+", type=str, required=True,
+    help="input file names", metavar="FILE")
+  parser.add_argument("-o", type=str, required=True,
+    help="destination directory", metavar="DST_DIR")
+
+  args = parser.parse_args()
+
+  return (args.action, args.o, args.i)
+
+def save(dst_dir, file_name, arr):
+  file_name = path.join(dst_dir, path.splitext(path.basename(file_name))[0])
+
+  np.save(file_name, arr)
+  T.save(T.tensor(arr), file_name+".T")
+
+  print "saved %s\tshape: %s" % (file_name, arr.shape)
+
+def mnist(file_names, dst_dir):
+  for file_name in file_names:
+    with open(file_name, "rb") as file:
+      # >: big-endian
+      # H: ushort (2 bytes)
+      # B: uchar (1 byte)
+      # I: uint (4 bytes)
+      zeros, dtype, dims = struct.unpack(">HBB", file.read(4))
+      shape = [struct.unpack(">I", file.read(4))[0] for dim in range(dims)]
+
+      # mnist input files known to have uint8 dtype
+      arr = np.frombuffer(file.read(), dtype=np.uint8).reshape(shape)
+
+      save(dst_dir, file_name, arr)
+
+def vids(file_names, dst_dir):
+  for file_name in file_names:
+    vid = cv2.VideoCapture(file_name)
+
+    num_frames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+    width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    vid_arr = np.empty((num_frames, height, width, 3), dtype=np.uint8)
+
+    for frame_num in xrange(num_frames):
+      ret, frame = vid.read()
+
+      if ret:
+        vid_arr[frame_num] = frame
+      else:
+        print "error in %s: frame [%d/%d]" % (file_name, frame_num, num_frames)
+        break
+
+    vid.release()
+
+    save(dst_dir, file_name, vid_arr)
+
+# ex:
+# python src/data.py mnist -i mnist/src/*-ubyte -o mnist/
+# python src/data.py vids -i vids/src/a4.sax.mov -o vids/
+if __name__ == "__main__":
+  action, dst_dir, file_names = parse_args()
+
+  {"mnist": mnist, "vids": vids}[action](file_names, dst_dir)
--- src/keras_ae.py
+++ src/keras_ae.py
@@ -1,9 +1,12 @@
 import argparse
-from keras.layers import Input, Dense
-from keras.models import Model
-# from keras.datasets import mnist
+
+import keras.activations as KA
+import keras.layers as KN
+import keras.losses as KL
+import keras.models as KM
+import keras.optimizers as KO
+
 import numpy as np
-import torch as T
 
 def parse_args():
   parser = argparse.ArgumentParser()
@@ -27,35 +30,54 @@ def parse_args():
 
   return parser.parse_args()
 
-def AE():
-  input_img = Input(shape=(784,))
-  encoded = Dense(128, activation='relu')(input_img)
-  encoded = Dense(64, activation='relu')(encoded)
-  encoded = Dense(32, activation='relu')(encoded)
+# def AE():
+#   input_img = Input(shape=(784,))
+#   encoded = Dense(128, activation='relu')(input_img)
+#   encoded = Dense(64, activation='relu')(encoded)
+#   encoded = Dense(32, activation='relu')(encoded)
 
-  decoded = Dense(64, activation='relu')(encoded)
-  decoded = Dense(128, activation='relu')(decoded)
-  decoded = Dense(784, activation='sigmoid')(decoded)
+#   decoded = Dense(64, activation='relu')(encoded)
+#   decoded = Dense(128, activation='relu')(decoded)
+#   decoded = Dense(784, activation='sigmoid')(decoded)
 
-  model = Model(input_img, decoded)
+#   model = Model(input_img, decoded)
 
-  model.compile(optimizer='adadelta', loss='binary_crossentropy')
+#   model.compile(optimizer='adadelta', loss='binary_crossentropy')
 
   return model
 
+def AE():
+  enc = KM.Sequential()
+  dec = KM.Sequential()
+  ae = KM.Sequential()
+
+  enc.add(KN.Dense(128, activation=KA.relu))
+  enc.add(KN.Dense(64, activation=KA.relu))
+  enc.add(KN.Dense(32, activation=KA.relu))
+
+  dec.add(KN.Dense(64, activation=KA.relu))
+  dec.add(KN.Dense(128, activation=KA.relu))
+  dec.add(KN.Dense(784, activation=KA.sigmoid))
+
+  ae.add(enc)
+  ae.add(dec)
+
+  ae.compile(optimizer=KO.Adadelta, loss=KL.binary_crossentropy)
+
+  return ae
+
 def get_data(file_name):
-  X = T.load(file_name).numpy().astype(np.float64)/255
+  X = np.load(file_name).astype(np.float64)/255
 
   return (X, X.reshape((X.shape[0], -1)))
 
+# python src/keras_ae.py -s -b256 -n50 -i mnist/train-images-idx3-ubyte.npy -j mnist/t10k-images-idx3-ubyte.npy
 if __name__ == "__main__":
   args = parse_args()
 
-  x_trn = get_data(args.train)
-  x_tst = get_data(args.test)
+  x_trn, xf_trn = get_data(args.train)
+  x_tst, xf_tst = get_data(args.test)
 
   ae = AE()
-  ae.fit(x_trn, x_trn, epochs=args.num_epochs, batch_size=args.batch_size,
-    shuffle=args.shuffle, validation_data=(x_tst, x_tst))
-
-
+  ae.fit(xf_trn, xf_trn, epochs=args.num_epochs, batch_size=args.batch_size,
+    shuffle=args.shuffle, validation_data=(xf_tst, xf_tst))
