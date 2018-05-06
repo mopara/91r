0f19f6cc96e6e2ef465be11d9fa225845b4cdf23
Michael Opara <opara@college.harvard.edu>
2018-05-06 14:07:07 -0400

    commit at Sun May  6 14:07:07 EDT 2018

--- src/keras_ae.py
+++ src/keras_ae.py
@@ -1,7 +1,7 @@
 import argparse
 
 # import keras.activations as KA
-import keras.layers as KN
+import keras.layers as KL
 # import keras.losses as KL
 import keras.models as KM
 # import keras.optimizers as KO
@@ -35,19 +35,19 @@ def AE1(D_in, D_out):
   dec = KM.Sequential()
   ae = KM.Sequential()
 
-  enc.add(KN.Dense(128, activation=KA.relu, input_shape=(D_in,)))
-  enc.add(KN.Dense(64, activation=KA.relu))
-  enc.add(KN.Dense(32, activation=KA.relu))
+  enc.add(KL.Dense(128, activation="relu", input_shape=(D_in,)))
+  enc.add(KL.Dense(64, activation="relu"))
+  enc.add(KL.Dense(32, activation="relu"))
 
-  dec.add(KN.Dense(64, activation=KA.relu, input_shape=(32,)))
-  dec.add(KN.Dense(128, activation=KA.relu))
-  dec.add(KN.Dense(D_out, activation=KA.sigmoid))
+  dec.add(KL.Dense(64, activation="relu", input_shape=(32,)))
+  dec.add(KL.Dense(128, activation="relu"))
+  dec.add(KL.Dense(D_out, activation="sigmoid"))
 
   ae.add(enc)
   ae.add(dec)
 
-  ae.compile(optimizer=KO.Adadelta(), loss=KL.binary_crossentropy)
-  # ae.compile(optimizer=KO.Adam(), loss=KL.binary_crossentropy)
+  ae.compile(optimizer="adadelta", loss="binary_crossentropy")
+  # ae.compile(optimizer="adam", loss="binary_crossentropy")
 
   return ae
 
@@ -80,10 +80,12 @@ def AE2(H, W):
 
   return ae
 
-def AE3(H):
-  enc = KM.Sequential()
+def AE3(D_in, H1, H2):
+  h = KM.Sequential()
+  h.add(KL.Dense(H1, activation="relu", input_shape=(D_in,)))
 
-  enc.add(KN.Dense())
+  mu = KM.Sequential()
+  mu.add(h)
 
 def get_data(file_name):
   X = np.load(file_name).astype(np.float64)/255
