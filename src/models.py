d5ee9802b748094c8106b1b563882bb62a3a1d3d
Michael Opara <opara@college.harvard.edu>
2018-05-06 21:46:05 -0400

    commit at Sun May  6 21:46:05 EDT 2018

--- src/models.py
+++ src/models.py
@@ -4,58 +4,44 @@ import torch.nn as nn
 import torch.nn.functional as f
 import torch.optim as optim
 
-class VAE(object):
-  def encode(self, x):
-    h = self.pre(x)
-
-    return (self.mu_fc(h), self.log_sigma_fc(h))
-
-  def sample(self, mu, log_sigma):
-    if self.training:
-      return t.randn_like(log_sigma).mul_((0.5*log_sigma).exp_()).add_(mu)
-    else:
-      return mu
-
-  def forward(self, x, y):
-    mu, log_sigma = self.encode(x)
-    y_prd = self.decode(self.sample(mu, log_sigma))
-
-    bce = f.binary_cross_entropy(y_prd, y, size_average=False)
-    kld = -0.5*(1+log_sigma).sub_(mu.pow(2)).sub_(log_sigma.exp()).sum()
-
-    return (y_prd, bce+kld)
-
-class VAE1(VAE, nn.Module):
+class VAE1(nn.Module):
   def __init__(self, D_in, D_hidden, D_latent):
     super(VAE1, self).__init__()
 
     self.pre = pre = nn.Sequential(
       nn.Linear(D_in, D_hidden),
       nn.ReLU())
-    self.mu_fc = mu_fc = nn.Linear(D_hidden, D_latent)
-    self.log_sigma_fc = log_sigma_fc = nn.Linear(D_hidden, D_latent)
+    self.mean_fc = mean_fc = nn.Linear(D_hidden, D_latent)
+    self.log_var_fc = log_var_fc = nn.Linear(D_hidden, D_latent)
     self.decode = decode = nn.Sequential(
       nn.Linear(D_latent, D_hidden),
       nn.ReLU(),
       nn.Linear(D_hidden, D_in),
       nn.Sigmoid())
-    self.opt = optim.Adam(it.chain(self.pre.parameters(), mu_fc.parameters(),
-      log_sigma_fc.parameters(), decode.parameters()), lr=1e-3)
+    self.opt = optim.Adam(it.chain(self.pre.parameters(), mean_fc.parameters(),
+      log_var_fc.parameters(), decode.parameters()), lr=1e-3)
 
-#  (5537, 240, 320, 3)
+  def encode(self, x):
+    h = self.pre(x)
 
-class Flatten(nn.Module):
-  def forward(self, x):
-    return x.view(x.size(0), -1)
+    return (self.mean_fc(h), self.log_var_fc(h))
 
-class Unflatten(nn.Module):
-  def __init__(self, *shape):
-    self.shape = shape
+  def sample(self, mean, log_var):
+    if self.training:
+      return t.randn_like(log_var).mul_((0.5*log_var).exp_()).add_(mean)
+    else:
+      return mean
 
-  def forward(self, x):
-    return x.view(*self.shape)
+  def forward(self, x, y):
+    mean, log_var = self.encode(x)
+    y_prd = self.decode(self.sample(mean, log_var))
 
-class VAE2(VAE, nn.Module):
+    bce = f.binary_cross_entropy(y_prd, y, size_average=False)
+    kld = -0.5*(1+log_var).sub_(mean.pow(2)).sub_(log_var.exp()).sum()
+
+    return (y_prd, bce+kld)
+
+class VAE2(nn.Module):
   def __init__(self, C_in, C1, C2, D_latent):
     super(VAE2, self).__init__()
 
@@ -65,13 +51,11 @@ class VAE2(VAE, nn.Module):
       nn.MaxPool2d(2, padding=0), # 240, 320 -> 120, 160
       nn.Conv2d(C1, 1, 3, padding=1),
       nn.ReLU(),
-      nn.MaxPool2d(2, padding=0), # 120, 160 -> 60, 80
-      Flatten())
-    self.mu_fc = mu_fc = nn.Linear(60*80, D_latent)
-    self.log_sigma_fc = log_sigma_fc = nn.Linear(60*80, D_latent)
+      nn.MaxPool2d(2, padding=0)) # 120, 160 -> 60, 80
+    self.mean_fc = mean_fc = nn.Linear(60*80, D_latent)
+    self.log_var_fc = log_var_fc = nn.Linear(60*80, D_latent)
+    self.z_fc = z_fc = nn.Linear(D_latent, 60*80)
     self.decode = decode = nn.Sequential(
-      nn.Linear(D_latent, 60*80),
-      Unflatten(1, 60, 80),
       nn.Conv2d(1, C2, 3, padding=1),
       nn.ReLU(),
       nn.Upsample(scale_factor=2, mode="nearest"), # 60, 80 -> 120, 160
@@ -80,5 +64,30 @@ class VAE2(VAE, nn.Module):
       nn.Upsample(scale_factor=2, mode="nearest"), # 120, 160 -> 240, 320
       nn.Conv2d(C1, C_in, 3, padding=1),
       nn.Sigmoid())
-    self.opt = optim.Adam(it.chain(self.pre.parameters(), mu_fc.parameters(),
-      log_sigma_fc.parameters(), decode.parameters()), lr=1e-3)
+    self.opt = optim.Adam(it.chain(self.pre.parameters(), mean_fc.parameters(),
+      log_var_fc.parameters(), z_fc.parameters(), decode.parameters()),
+      lr=1e-3)
+
+  def encode(self, x):
+    h = self.pre(x)
+
+    h.resize_(h.size(0), -1)
+
+    return (self.mean_fc(h), self.log_var_fc(h))
+
+  def sample(self, mean, log_var):
+    if self.training:
+      return t.randn_like(log_var).mul_((0.5*log_var).exp_()).add_(mean)
+    else:
+      return mean
+
+  def forward(x):
+    mean, log_var = self.encode(x)
+    y_prd = self.decode(self.z_fc(self.sample(mean, log_var)).resize_(1, 60,
+      80))
+
+    bce = f.binary_cross_entropy(y_prd, y, size_average=False)
+    kld = -0.5*(1+log_var).sub_(mean.pow(2)).sub_(log_var.exp()).sum()
+
+    return (y_prd, bce+kld)
+
