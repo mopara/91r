05fb8fb9f15fbc5c71ae25a105a24ad55c0a0498
Michael Opara <opara@college.harvard.edu>
2018-05-07 07:57:48 -0400

    commit at Mon May  7 07:57:48 EDT 2018

--- src/main.py
+++ src/main.py
@@ -117,7 +117,7 @@ if __name__ == "__main__":
         "vae2": models.VAE2(D, 128, 64, 32, D_z),
         # "cvae": models.CVAE(H, W, C, 64, 128, D_z),
         "cvae": models.CVAE(H, W, C, 8, 32, D_z),
-        "infovae": models.InfoVAE(H, W, C, 64, 128, 1024, D_z)}:
+        "infovae": models.InfoVAE(H, W, C, 64, 128, 1024, D_z)}.items():
 
         print ">>>>>>>>>"
         print train_file, test_file
