f10bc0d1d3eed9dc62f0f984fdff7f6bfaf8da85
Michael Opara <opara@college.harvard.edu>
2018-05-07 02:17:47 -0400

    commit at Mon May  7 02:17:47 EDT 2018

--- src/main.py
+++ src/main.py
@@ -85,13 +85,12 @@ if __name__ == "__main__":
   D = H * W * C
 
   # vae = models.VAE1(D, 400, 20).to(device)
-  # vae = models.VAE2(H, W, C, 16, 8, 20).to(device)
   # vae = models.VAE3(H, W, C, 64, 128, 2).to(device)
   vae = models.VAE3(H, W, C, 8, 32, 2).to(device)
 
-  train(vae, get_batches(xf, xf, args.batch_size, args.shuffle), args.epochs)
+  train(vae, get_batches(xc, xc, args.batch_size, args.shuffle), args.epochs)
 
   if args.test:
     x, xf, xc = get_data(args.test, device)
 
-    test(vae, get_batches(xf, xf, args.batch_size, args.shuffle))
+    test(vae, get_batches(xc, xc, args.batch_size, args.shuffle))
